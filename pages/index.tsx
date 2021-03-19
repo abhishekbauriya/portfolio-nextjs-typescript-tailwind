83fce91cd95626fea485cf250efb4e36ec03bd98
abhishekbauriya <abhishekbauriya007@gmail.com>
2021-03-19 15:26:10 +0530

    Added services

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
-const index = () => {
+import { GetServerSidePropsContext, GetStaticPropsContext } from 'next'
+import { services } from '../data'
+
+const index = ({services}) => {
+  console.log(services)
+
   return (
     <div>
       <h1>Hello</h1>
@@ -7,3 +12,41 @@ const index = () => {
 }
 
 export default index
+
+
+// export const getServerSideProps = async (
+//   context:GetServerSidePropsContext
+// ) => {
+  
+//   //calculation
+
+//   const res = await fetch('http://localhost:3000/api/services')
+//   const data = await res.json()
+
+//   console.log('SERVER', services);
+
+//   return {
+//     props: {
+//       services: data.services,
+//     },
+//   }
+// }
+
+
+// export const getStaticProps = async (
+//   context: GetStaticPropsContext
+// ) => {
+  
+//   //calculation
+
+//   const res = await fetch('http://localhost:3000/api/services')
+//   const data = await res.json()
+
+//   console.log('SERVER', services);
+
+//   return {
+//     props: {
+//       services: data.services,
+//     },
+//   }
+// }
