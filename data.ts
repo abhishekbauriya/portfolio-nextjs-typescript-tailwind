e3c13a95fdd61499b6df36b3b98d320f631fb406
abhishekbauriya <abhishekbauriya007@gmail.com>
2021-03-21 16:03:59 +0530

    Added Project Part

--- data.ts
+++ data.ts
@@ -108,7 +108,7 @@ export const projects:IProject[] = [
       image_path: "/images/covid.jpg",
       deployed_url: "https://covid-19-tracker-by-sumit.web.app/",
       github_url: "https://github.com/Dey-Sumit/covid-19-tracker",
-      category: ["react"],
+      category: ["REACT.Js"],
       key_techs: ["React", "Chart.js", "Material UI"],
    },
    {
@@ -117,7 +117,7 @@ export const projects:IProject[] = [
       image_path: "/images/algoVisual.png",
       deployed_url: "https://visual-algorithm.web.app/",
       github_url: "https://github.com/Dey-Sumit/algorithm-visualizer",
-      category: ["react"],
+      category: ["REACT.Js"],
       description:
          "An web app which shows how an algorithm (path finding or sorting) works with cool animation",
       key_techs: ["React", "firebase", "Framer Motion"],
@@ -127,7 +127,7 @@ export const projects:IProject[] = [
       image_path: "/images/dev.jpg",
       deployed_url: "https://dev-talks.herokuapp.com/",
       github_url: "https://github.com/Dey-Sumit/Dev-talks",
-      category: ["node", "mongo", "react"],
+      category: ["MERN", "REACT.Js"],
       description:
          "Social Media app for developers who can share project,create posts,etc...",
       key_techs: ["React", "Redux", "Node", "Express", "Mongo", "REST API", "Bootstrap",],
@@ -138,7 +138,7 @@ export const projects:IProject[] = [
       deployed_url: "https://sumit-chat.netlify.app/",
       github_url: "https://github.com/Dey-Sumit/chat-app-socket.io-react-node",
       //TODO type on category
-      category: ["node", "react"],
+      category: ["REACT.Js", "Python"],
       description:
          "Basic Realtime Chat App where one can create a room can talk to each other",
       key_techs: ["React", "Node", "Express", "Socket", "Bootstrap"],
@@ -148,7 +148,7 @@ export const projects:IProject[] = [
       image_path: "/images/tweetme.jpg",
       deployed_url: "http://sumaxtweetme.pythonanywhere.com/",
       github_url: "https://github.com/Dey-Sumit/tweetme",
-      category: ["django", "react"],
+      category: ["Django", "REACT.Js"],
       description:
          "First Django Project :) | Typical Social Media App where one can post,like ,comment etc",
       key_techs: ["React", "Django", "Django REST API"],
@@ -157,7 +157,7 @@ export const projects:IProject[] = [
       name: "Dev Portfolio",
       image_path: "/images/portfolio.jpg",
       deployed_url: "http://suprateem.herokuapp.com/",
-      category: ["vanilla"],
+      category: ["REACT.Js"],
       github_url: "https://github.com/Dey-Sumit/tweetme",
       description: "Hey, You are seeing this project now...Enjoy",
       key_techs: ["Next.js", "Framer Motion", "TypeScript", "Tailwind"],
@@ -167,17 +167,17 @@ export const projects:IProject[] = [
       image_path: "/images/color.jpg",
       deployed_url: "!#",
       github_url: "https://github.com/Dey-Sumit/color-classification",
-      category: ["express", "ml"],
+      category: ["MERN", "ml"],
       description:
          "Tried ML with JS :) | this app classifies a color using CNN algorithm in browser",
-      key_techs: ["Express", "TensorFlow.js", "Vanilla js"],
+      key_techs: ["MERN", "TensorFlow.js", "Vanilla js"],
    },
    {
       name: "YouTube using YouTube ",
       image_path: "/images/youtubeClone.png",
       deployed_url: "https://not-utube.web.app/",
       github_url: "https://github.com/Dey-Sumit/youtube-clone-tutorial-up",
-      category: ["express", "ml"],
+      category: ["MERN", "ml"],
       description:
          'Full(almost) Functional YouTube replica where one can login with his/her youtube account to enjoy "not-YouTube".User can like a video,comment on a video & Much More ',
       key_techs: ["React", "Redux", "Firebase Auth", "YouTube API", "Sass", "Bootstrap",],
@@ -188,7 +188,7 @@ export const projects:IProject[] = [
       image_path: "/images/football.png",
       deployed_url: "https://o-my-goal.web.app/",
       github_url: "https://github.com/Dey-Sumit/football-app",
-      category: ["react"],
+      category: ["React Native"],
       description:
          "o my goal replica where an user can keep an eye on his favorite club.This app will keep providing \n all the statistics of that club.all the fans can also chat ",
       key_techs: ["React", "Redux", "Firebase Auth", "API", "Sass", "Bootstrap"],
